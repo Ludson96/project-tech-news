49e33ef8ff6cc24f068f17e1b1c2ba88dde562f9
Ludson96 <ludson_ps27@hotmail.com>
2023-04-17 14:59:48 -0300

    feat: :sparkles: add função scrape_news
    função responsavel por retornar relação de atributos da pagina

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -35,19 +35,37 @@ def scrape_updates(html_content: HTML) -> list:
 
 
 # Requisito 3
-def scrape_next_page_link(html_content):
+def scrape_next_page_link(html_content: HTML) -> None | str:
     try:
         selector = Selector(text=html_content)
-        next_page = selector.css(".next.page-numbers::attr(href)").get()
+        link_next_page = selector.css(".next.page-numbers::attr(href)").get()
     except ValueError:
         return None
     else:
-        return next_page
+        return link_next_page
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    summary_content = selector.css(
+        "div.entry-content > p:first-of-type *::text"
+    ).getall()
+    summary = "".join(summary_content).strip()
+
+    info_noticias = {
+        "url": selector.css("link[rel='canonical']::attr(href)").get(),
+        "title": selector.css(".entry-title::text").get().strip(),
+        "timestamp": selector.css(".meta-date::text").get(),
+        "writer": selector.css(".url.fn.n::text").get(),
+        "reading_time": int(
+            selector.css(".meta-reading-time::text").re_first(r"\d+")
+        ),
+        "summary": summary,
+        "category": selector.css(".label::text").get(),
+    }
+    return info_noticias
 
 
 # Requisito 5
