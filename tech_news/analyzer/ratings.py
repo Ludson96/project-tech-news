3d31c2b045f3247fa70266e695984ed8eec0d7b5
Ludson96 <ludson_ps27@hotmail.com>
2023-04-18 11:56:08 -0300

    feat: :sparkles: add top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,15 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    pipeline = [
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$project": {"categoria": "$_id", "count": 1, "_id": 0}},
+        {"$sort": {"count": -1, "categoria": 1}},
+        {"$limit": 5}
+    ]
+
+    news = db.news.aggregate(pipeline)
+    categories = [new["categoria"] for new in news]
+    return categories
